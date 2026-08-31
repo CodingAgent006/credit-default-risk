fcbfea5dde9bbe71acd7cb9877cf3434bc96f422
CodingAgent006 <122568543+CodingAgent006@users.noreply.github.com>
2026-08-31 12:21:41 -0500

    Update test_features.py

--- tests/test_features.py
+++ tests/test_features.py
@@ -2,21 +2,12 @@ import pandas as pd
 from src.features import build_preprocessor
 
 def test_build_preprocessor_execution():
-    sample_data = pd.DataFrame([{
-        'person_age': 25,
-        'person_income': 50000,
-        'person_emp_length': 3.0,
-        'loan_amnt': 10000,
-        'loan_int_rate': 10.5,
-        'loan_percent_income': 0.2,
-        'cb_person_cred_hist_length': 4,
-        'person_home_ownership': 'RENT',
-        'loan_intent': 'PERSONAL',
-        'loan_grade': 'A',
-        'cb_person_default_on_file': 'N'
+    sample = pd.DataFrame([{
+        'person_age': 25, 'person_income': 50000, 'person_emp_length': 3.0,
+        'loan_amnt': 10000, 'loan_int_rate': 10.5, 'loan_percent_income': 0.2,
+        'cb_person_cred_hist_length': 4, 'person_home_ownership': 'RENT',
+        'loan_intent': 'PERSONAL', 'loan_grade': 'A', 'cb_person_default_on_file': 'N'
     }])
-
     preprocessor = build_preprocessor()
-    transformed = preprocessor.fit_transform(sample_data)
+    transformed = preprocessor.fit_transform(sample)
     assert transformed.shape[0] == 1
-    assert transformed.shape[1] > 0
